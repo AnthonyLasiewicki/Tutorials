c0e1a8cbb0a5cbb4b1c942ee939da62f7a94bbbc
nano1452 <Git_nano1452@yahoo.com>
2015-03-09 18:33:48 -0400

    Few Changes

--- src/com/nano/Tutorials/GUI/RadioButtonMain.java
+++ src/com/nano/Tutorials/GUI/RadioButtonMain.java
@@ -1,9 +1,14 @@
 package com.nano.Tutorials.GUI;
 
-public class RadioButtonMain {
+import javax.swing.JFrame;
+
+public class RadioButtonMain extends JFrame{
 
 	public static void main(String[] args) {
-		
+		RadioButton2 window = new RadioButton2();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(300, 110);
+		window.setVisible(true);
 
 	}
 
--- src/com/nano/Tutorials/Misc/apples.java
+++ src/com/nano/Tutorials/Misc/apples.java
@@ -1,7 +1,13 @@
 package com.nano.Tutorials.Misc;
 
+import javax.swing.JFrame;
 
-public class apples {
+
+public class apples{
+	
 	public static void main(String[] args){}
+	
+	
+	
 	
 }
